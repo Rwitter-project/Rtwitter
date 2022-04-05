41abd63deb796834a42caa6e5d78c502393274d4
Joris P <71002181+Miisterone@users.noreply.github.com>
2022-04-05 12:07:30 +0200

    feat(unconnected): add username,first,last name,etc

--- db/migrate/20220315192816_devise_create_users.rb
+++ db/migrate/20220315192816_devise_create_users.rb
@@ -33,11 +33,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
 
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :description
+      t.string :localisation
+      t.string :certified
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :username,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     add_index :users, :unlock_token,         unique: true
