6215d450e0586ba145f54a47c7610be85d6a4a52
Raphael Arabeyre <raphael.arabeyre@ynov.com>
2022-04-12 10:39:27 +0200

    fix: fix sqlit bug

--- db/migrate/20220329095119_create_rweets.rb
+++
@@ -1,9 +0,0 @@
-class CreateRweets < ActiveRecord::Migration[7.0]
-  def change
-    create_table :rweets do |t|
-      t.belongs_to :user
-      t.string :body
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_29_095119) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_05_071154) do
   create_table "rweets", force: :cascade do |t|
     t.integer "user_id"
     t.string "body"
