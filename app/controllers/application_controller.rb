867884ed72a33114b6ee628d32dbde435ac590c5
Joris P <71002181+Miisterone@users.noreply.github.com>
2022-04-12 11:15:48 +0200

    feat(unconnected): add profile page + change confirmation email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :description, :first_name , :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :description, :first_name , :last_name, :location])
+    devise_parameter_sanitizer.permit(:edit_user_registration_path, keys: [:username, :description, :first_name , :last_name, :location])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'logout' => 'sessions#destroy'
+  get 'profil' => 'welcome#profil'
 end
