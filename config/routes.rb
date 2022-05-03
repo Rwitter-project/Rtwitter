567750d9e8bc9b8d0a6424739013476a2686ddb9
Willd27 <48601897+Willd27@users.noreply.github.com>
2022-05-03 22:25:32 +0200

    fix: destroy and like visibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
+
+  resources :users
+
   resources :rweets do
     resources :likes
   end
 
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
