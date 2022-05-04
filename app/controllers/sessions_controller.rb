00e9768e7fb6ee7dc33a9d70e0e557cbdda7ac09
Miisterone <miisterone27@gmail.com>
2022-05-05 01:44:33 +0200

    feat(front-profile): change front

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,6 @@ class SessionsController < ApplicationController
     session.destroy
     # Delete cookie to log out even with remember me
     cookies.delete :remember_user_token
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url
   end
 end
