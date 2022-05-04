88fcd722f62f232cb0f2b5af2efc781c4040a5fa
Willd27 <48601897+Willd27@users.noreply.github.com>
2022-05-04 10:00:31 +0200

    feat: add comments and begin of reply

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   get 'welcome/index'
 
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :rweets do
     resources :likes
+    resources :comments
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
